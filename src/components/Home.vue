286aed60e5330f9e0895b662289e1f4d04e5a381
Robert Herber <rwherber@gmail.com>
2020-04-25 14:50:15 -0700

    fix: fix base url and update dependencies

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,174 +1,227 @@
 <template>
-    <FdTabs v-model="activeTab">
-        <FdTabItem striped label="Budget Tool" name="a" class="content">
-            
-            <div v-if="budget.getMonthlyExpenses().length > 0">
-                <h2>Monthly Expenses</h2>
-                <FdTable striped :headers="budgetHeaders" :items="budget.getMonthlyExpenses()">
-                    <template slot="row" slot-scope="{ item }">
-                        <FdTableRow @click="goToExpense(item.name)">
-                            <FdTableCell>{{ item.name }}</FdTableCell>
-                            <FdTableCell>${{ item.expense }}</FdTableCell>
-                            <FdTableCell><FdButton type="negative" state="normal" @click.stop="budget.removeMonthlyExpense(item)">Delete</FdButton></FdTableCell>
-                        </FdTableRow>
-                    </template>
-                </FdTable>
-                <h2>Total Monthly Per Month: ${{ budget.getMonthlySum() }} (${{ budget.getMonthlySum() * 12 }} per year)</h2>
-            </div>
+  <FdTabs v-model="activeTab">
+    <FdTabItem striped label="Budget Tool" name="a" class="content">
+      <div v-if="budget.getMonthlyExpenses().length > 0">
+        <h2>Monthly Expenses</h2>
+        <FdTable
+          striped
+          :headers="budgetHeaders"
+          :items="budget.getMonthlyExpenses()"
+        >
+          <template slot="row" slot-scope="{ item }">
+            <FdTableRow @click="goToExpense(item.name)">
+              <FdTableCell>{{ item.name }}</FdTableCell>
+              <FdTableCell>${{ item.expense }}</FdTableCell>
+              <FdTableCell
+                ><FdButton
+                  type="negative"
+                  state="normal"
+                  @click.stop="budget.removeMonthlyExpense(item)"
+                  >Delete</FdButton
+                ></FdTableCell
+              >
+            </FdTableRow>
+          </template>
+        </FdTable>
+        <h2>
+          Total Monthly Per Month: ${{ budget.getMonthlySum() }} (${{
+            budget.getMonthlySum() * 12
+          }}
+          per year)
+        </h2>
+      </div>
 
-            <hr>
-            <br>
+      <hr />
+      <br />
 
-            <div v-if="budget.getAnnualExpenses().length > 0">
-                <h2>Annual Expenses</h2>
-                <FdTable striped :headers="budgetHeaders" :items="budget.getAnnualExpenses()">
-                    <template slot="row" slot-scope="{ item }">
-                        <FdTableRow @click="goToExpense(item.name)">
-                            <FdTableCell>{{ item.name }}</FdTableCell>
-                            <FdTableCell>${{ item.expense }}</FdTableCell>
-                            <FdTableCell><FdButton type="negative" state="normal" @click.stop="budget.removeAnnualExpense(item)">Delete</FdButton></FdTableCell>
-                        </FdTableRow>
-                    </template>
-                </FdTable>
-                <h2>Total Annual Per Year: ${{ budget.getAnnualSum() }} (${{ (budget.getAnnualSum() / 12).toFixed(2) }} per month)</h2>
-            </div>
+      <div v-if="budget.getAnnualExpenses().length > 0">
+        <h2>Annual Expenses</h2>
+        <FdTable
+          striped
+          :headers="budgetHeaders"
+          :items="budget.getAnnualExpenses()"
+        >
+          <template slot="row" slot-scope="{ item }">
+            <FdTableRow @click="goToExpense(item.name)">
+              <FdTableCell>{{ item.name }}</FdTableCell>
+              <FdTableCell>${{ item.expense }}</FdTableCell>
+              <FdTableCell
+                ><FdButton
+                  type="negative"
+                  state="normal"
+                  @click.stop="budget.removeAnnualExpense(item)"
+                  >Delete</FdButton
+                ></FdTableCell
+              >
+            </FdTableRow>
+          </template>
+        </FdTable>
+        <h2>
+          Total Annual Per Year: ${{ budget.getAnnualSum() }} (${{
+            (budget.getAnnualSum() / 12).toFixed(2)
+          }}
+          per month)
+        </h2>
+      </div>
 
-            <hr>
+      <hr />
 
-            <br>
-            <h2>Total Yearly Expenses: ${{ budget.getTotalAll() }}</h2>
-            <hr>
-            <h2>Annual Income: $75000</h2>
-            <h2>Remaining Budget: ${{ budget.getRemainingBudget() }}</h2>
-            <h2>Remaining Budget Per Month: ${{ budget.getRemainingMonthlyBudget() }}</h2>
-            <h2>Remaining Budget Per Week: ${{ budget.getRemainingWeeklyBudget() }}</h2>
-            <br>
-            
-            <hr>
+      <br />
+      <h2>Total Yearly Expenses: ${{ budget.getTotalAll() }}</h2>
+      <hr />
+      <h2>Annual Income: $75000</h2>
+      <h2>Remaining Budget: ${{ budget.getRemainingBudget() }}</h2>
+      <h2>
+        Remaining Budget Per Month: ${{ budget.getRemainingMonthlyBudget() }}
+      </h2>
+      <h2>
+        Remaining Budget Per Week: ${{ budget.getRemainingWeeklyBudget() }}
+      </h2>
+      <br />
 
-            <FdButton @click.stop="openAddExpense">Add Expense</FdButton>
-            <FdButton @click="resetAPIDataToDefault()">Set Default Budget</FdButton>
-            <FdModal title="Add Expense" :active.sync="showAddExpense">
-                <p>Add an expense.</p>
-                <FdFormSet>
-                    <FdFormItem label="Name">
-                        <FdInput placeholder="Expense Name" :state="nameState === '' ? null : nameState" v-model="newName" />
-                    </FdFormItem>
-                    <FdFormItem label="Cost">
-                        <FdInput placeholder="Expense Cost" type="number" :state="costState === '' ? null : costState" v-model="newCost" />
-                    </FdFormItem>
-                    <FdFormItem label="Type">
-                        <FdSelect v-model="selectType" :state="typeState === '' ? null : typeState">
-                            <option value="Annual">Annual</option>
-                            <option value="Monthly">Monthly</option>
-                        </FdSelect>
-                    </FdFormItem>
-                </FdFormSet>
-                <template slot="actions">
-                    <FdButton @click="closeAddExpense()" styling="light">Cancel</FdButton>
-                    <FdButton @click="addExpense()" styling="emphasized">Add Expense</FdButton>
-                </template>
-            </FdModal>
-        </FdTabItem>
-    </FdTabs>
+      <hr />
+
+      <FdButton @click.stop="openAddExpense">Add Expense</FdButton>
+      <FdButton @click="resetAPIDataToDefault()">Set Default Budget</FdButton>
+      <FdModal title="Add Expense" :active.sync="showAddExpense">
+        <p>Add an expense.</p>
+        <FdFormSet>
+          <FdFormItem label="Name">
+            <FdInput
+              placeholder="Expense Name"
+              :state="nameState === '' ? null : nameState"
+              v-model="newName"
+            />
+          </FdFormItem>
+          <FdFormItem label="Cost">
+            <FdInput
+              placeholder="Expense Cost"
+              type="number"
+              :state="costState === '' ? null : costState"
+              v-model="newCost"
+            />
+          </FdFormItem>
+          <FdFormItem label="Type">
+            <FdSelect
+              v-model="selectType"
+              :state="typeState === '' ? null : typeState"
+            >
+              <option value="Annual">Annual</option>
+              <option value="Monthly">Monthly</option>
+            </FdSelect>
+          </FdFormItem>
+        </FdFormSet>
+        <template slot="actions">
+          <FdButton @click="closeAddExpense()" styling="light">Cancel</FdButton>
+          <FdButton @click="addExpense()" styling="emphasized"
+            >Add Expense</FdButton
+          >
+        </template>
+      </FdModal>
+    </FdTabItem>
+  </FdTabs>
 </template>
 
-<script lang='ts'>
+<script lang="ts">
 import Vue, { VueConstructor } from "vue";
 
-import { mapGetters } from 'vuex';
+import { mapGetters } from "vuex";
 import { Budget } from "./../classes/Budget";
 import { Expense, ExpenseType } from "./../classes/Expense";
 import { API } from "./../services/API";
-import TestMixin from './TestMixin';
-import mixins, { VueClass } from 'vue-typed-mixins';
+import TestMixin from "./TestMixin";
+import mixins from "vue-typed-mixins";
 
+export default mixins(
+  TestMixin,
+  Vue as VueConstructor<Vue & { $refs: { test: string } }>
+).extend({
+  name: "app",
 
+  data() {
+    return {
+      budgetHeaders: [
+        { label: "Name", sortable: true, sortBy: "name" },
+        { label: "Expense", sortable: true, sortBy: "expense" },
+        { label: "Delete" },
+      ],
+      budget: Budget.Instance,
+      activeTab: "a",
+      showAddExpense: false,
+      selectType: "",
+      newName: "",
+      newCost: 0,
+      nameState: "",
+      costState: "",
+      typeState: "",
+    };
+  },
 
-export default mixins(TestMixin, (Vue as VueConstructor<Vue & {$refs: {test: string;}}>)).extend({
-    name: "app",
-
-    data() {
-        return {
-            budgetHeaders: [
-                { label: "Name", sortable: true, sortBy: "name"},
-                { label: "Expense", sortable: true, sortBy: "expense"},
-                { label: "Delete"},
-            ],
-            budget: Budget.Instance,
-            activeTab: "a",
-            showAddExpense: false,
-            selectType: "",
-            newName: "",
-            newCost: 0,
-            nameState: "",
-            costState: "",
-            typeState: "",
-        };
+  methods: {
+    openAddExpense(): void {
+      this.showAddExpense = true;
     },
 
-    methods: {
-        openAddExpense(): void {
-            this.showAddExpense = true;
-            
-        },
-
-        closeAddExpense(): void {
-            this.nameState = "";
-            this.costState = "";
-            this.typeState = "";
-            this.newCost = 0;
-            this.newName = "";
-            this.selectType = "";
-            this.showAddExpense = false;
-            this.$refs.test = 'butt';
-        },
-
-        addExpense(): void {
-            this.nameState = "";
-            this.costState = "";
-            this.typeState = "";
-
-            if (this.newName === "") {
-                this.nameState = "invalid";
-                return;
-            }
-            
-            if (<any>this.newCost === "" || this.newCost < 0) {
-                this.costState = "invalid";
-                return;
-            }
-
-            if (this.selectType === "") {
-                this.typeState = "invalid";
-                return;
-            }
-
-            if (this.selectType === "Annual") {
-                this.budget.addAnnualExpense(new Expense(this.newName, Number(this.newCost), ExpenseType.Annual));
-            } else {
-                this.budget.addMonthlyExpense(new Expense(this.newName, Number(this.newCost), ExpenseType.Monthly));
-            }
-
-            this.closeAddExpense();
-        },
-
-        goToExpense(id: string): void {
-            this.$router.push({name: 'expense', params: { id: id } });
-        },
-
-        resetAPIDataToDefault(): void {
-            API.post("https://rwherber.com/APIs/BudgetTool/index.php", {reset: true}).then((value) => {
-                this.budget.reloadAPIData();
-                alert("API data reset!");
-            });
-        }
+    closeAddExpense(): void {
+      this.nameState = "";
+      this.costState = "";
+      this.typeState = "";
+      this.newCost = 0;
+      this.newName = "";
+      this.selectType = "";
+      this.showAddExpense = false;
+      this.$refs.test = "butt";
     },
 
-    computed: {
-        ...mapGetters([
-            'doubleCounter'
-        ])
-    }
+    addExpense(): void {
+      this.nameState = "";
+      this.costState = "";
+      this.typeState = "";
+
+      if (this.newName === "") {
+        this.nameState = "invalid";
+        return;
+      }
+
+      if (<any>this.newCost === "" || this.newCost < 0) {
+        this.costState = "invalid";
+        return;
+      }
+
+      if (this.selectType === "") {
+        this.typeState = "invalid";
+        return;
+      }
+
+      if (this.selectType === "Annual") {
+        this.budget.addAnnualExpense(
+          new Expense(this.newName, Number(this.newCost), ExpenseType.Annual)
+        );
+      } else {
+        this.budget.addMonthlyExpense(
+          new Expense(this.newName, Number(this.newCost), ExpenseType.Monthly)
+        );
+      }
+
+      this.closeAddExpense();
+    },
+
+    goToExpense(id: string): void {
+      this.$router.push({ name: "expense", params: { id: id } });
+    },
+
+    resetAPIDataToDefault(): void {
+      API.post("https://rwherber.com/APIs/BudgetTool/index.php", {
+        reset: true,
+      }).then((value) => {
+        this.budget.reloadAPIData();
+        alert("API data reset!");
+      });
+    },
+  },
+
+  computed: {
+    ...mapGetters(["doubleCounter"]),
+  },
 });
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: '/BudgetTool/'
-}
+  publicPath: "/budget-tool/",
+};
